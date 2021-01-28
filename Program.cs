a3d4689dc2e9d881cabf2f947f84d669c6159943
Elvizs <cassiano.elvis30@gmail.com>
2021-01-28 18:37:17 -0300

    Comit teste

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DigitalInnovationOne
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int vezes = 5;
+
+            for (int i=0; i<vezes; i++){
+
+                Console.WriteLine(value: $"Bem vindo ao curso de .NET {i}");
+            }
+        }
+    }
+}
